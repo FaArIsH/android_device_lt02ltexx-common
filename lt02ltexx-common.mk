91ebfcfa6f8e7d9a58dfb049a5afd50e45e0da82
FaArIsH <faarishhassan@gmail.com>
2016-07-06 14:07:27 +0500

    fix vendor

--- lt02ltexx-common.mk
+++ lt02ltexx-common.mk
@@ -15,7 +15,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/serrano-common/serrano-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/wilcoxltexx/wilcoxltexx-vendor.mk)
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/lt02ltexx-common/overlay
